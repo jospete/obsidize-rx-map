e7978ec3e723317a47fad798135e32292e8b0582
Josh Noel <obsidize@gmail.com>
2021-07-23 17:56:22 -0500

    organize exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+export * from './common/operators';
+export * from './common/subsink';
+export * from './common/utility';
+
 export * from './events/change-detection-event';
+export * from './events/map-state-change-event';
+
 export * from './maps/entity-map';
 export * from './maps/immutable-map';
-export * from './events/map-state-change-event';
 export * from './maps/mutable-rx-entity-map';
-export * from './common/operators';
 export * from './maps/proxy-iterable-iterator';
 export * from './maps/rx-entity-map';
 export * from './maps/rx-map';
+
 export * from './rx-store';
-export * from './common/subsink';
-export * from './common/utility';
